ac97479a8291e1631c898d84612c28bbf9ecf0fd
Samsul Arifin <arifinsam.sam@gmail.com>
2020-10-31 13:28:27 +0700

    rename jitsi meet to qiscus meet ios

--- index.ios.js
+++ index.ios.js
@@ -4,18 +4,16 @@
 
 import { NativeModules, requireNativeComponent } from 'react-native';
 
-export const JitsiMeetView = requireNativeComponent('RNJitsiMeetView');
-export const JitsiMeetModule = NativeModules.RNJitsiMeetView;
-const call = JitsiMeetModule.call;
-const audioCall = JitsiMeetModule.audioCall;
-JitsiMeetModule.call = (url, userInfo) => {
+export const QiscusMeetView = requireNativeComponent('RNJitsiMeetView');
+export const QiscusMeetModule = NativeModules.RNJitsiMeetModule
+const call = QiscusMeetModule.call;
+const audioCall = QiscusMeetModule.audioCall;
+QiscusMeetModule.call = (url, userInfo) => {
   userInfo = userInfo || {};
   call(url, userInfo);
 }
-JitsiMeetModule.audioCall = (url, userInfo) => {
+QiscusMeetModule.audioCall = (url, userInfo) => {
   userInfo = userInfo || {};
   audioCall(url, userInfo);
 }
-export default JitsiMeetModule;
-
-
+export default QiscusMeetModule;
