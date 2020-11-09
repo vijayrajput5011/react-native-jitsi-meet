e6e205385b0ee4b7fd7db1b2d285fe325e13a5b9
Maulana <mprambadi@gmail.com>
2020-11-09 20:16:28 +0700

    Add generate token method

--- android/src/main/java/com/reactnativejitsimeet/RNJitsiMeetModule.java
+++ android/src/main/java/com/reactnativejitsimeet/RNJitsiMeetModule.java
@@ -4,12 +4,18 @@ import android.util.Log;
 import java.net.URL;
 import java.net.MalformedURLException;
 
+import java.util.HashMap;
+import java.util.Map;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.UiThreadUtil;
 import com.facebook.react.module.annotations.ReactModule;
 import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.uimanager.IllegalViewOperationException;
 
 @ReactModule(name = RNJitsiMeetModule.MODULE_NAME)
 public class RNJitsiMeetModule extends ReactContextBaseJavaModule {
@@ -30,6 +36,18 @@ public class RNJitsiMeetModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
+    public void getSetup(Promise promise) {
+        try {
+            WritableMap map = Arguments.createMap();
+             map.putString("appId", this.appId);
+             map.putString("url", this.url);
+            promise.resolve(map);
+        } catch (IllegalViewOperationException e) {
+            promise.reject("AppId and URL not set", e);
+        }
+    }
+
+    @ReactMethod 
     public void initialize() {
         Log.d("JitsiMeet", "Initialize is deprecated in v2");
     }
@@ -41,10 +59,10 @@ public class RNJitsiMeetModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void call(ReadableMap userInfo, String room, Boolean videoMuted, Boolean audioMuted) {
+    public void call(ReadableMap userInfo, String room, Boolean videoMuted, Boolean audioMuted, String token) {
         String appId = this.appId;
         String url = this.url;
-        String serverUrl = url + "/" + room;
+        String serverUrl = url + "/" + appId + "/" + room;
 
         if(appId == null && appId.trim().isEmpty() ) {
             Log.d("QiscusMeet", "Please setup appID first");
@@ -55,8 +73,6 @@ public class RNJitsiMeetModule extends ReactContextBaseJavaModule {
             Log.d("QiscusMeet","Please setup server URL first");
             return;
         }
-
-
 
         UiThreadUtil.runOnUiThread(new Runnable() {
             @Override
@@ -81,6 +97,7 @@ public class RNJitsiMeetModule extends ReactContextBaseJavaModule {
                     RNJitsiMeetConferenceOptions options = new RNJitsiMeetConferenceOptions.Builder()
                             .setRoom(serverUrl)
                             .setAudioOnly(false)
+                            .setToken(token)
                             .setUserInfo(_userInfo)
                             .setVideoMuted(videoMuted)
                             .setAudioMuted(audioMuted)
@@ -92,10 +109,10 @@ public class RNJitsiMeetModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void audioCall(ReadableMap userInfo, String room, Boolean audioMuted) {
+    public void audioCall(ReadableMap userInfo, String room, Boolean audioMuted, String token) {
        String appId = this.appId;
        String url = this.url;
-       String serverUrl = url + room;
+       String serverUrl = url + "/" + appId + "/" + room;
 
         if(appId == null && appId.trim().isEmpty() ) {
             Log.d("QiscusMeet","Please setup appID first");
@@ -131,6 +148,7 @@ public class RNJitsiMeetModule extends ReactContextBaseJavaModule {
                     RNJitsiMeetConferenceOptions options = new RNJitsiMeetConferenceOptions.Builder()
                             .setRoom(serverUrl)
                             .setAudioOnly(true)
+                            .setToken(token)
                             .setUserInfo(_userInfo)
                             .setAudioMuted(audioMuted)
                             .build();
--- index.android.js
+++ index.android.js
@@ -9,20 +9,50 @@ export const QiscusMeetModule = NativeModules.RNJitsiMeetModule;
 const call = QiscusMeetModule.call;
 const audioCall = QiscusMeetModule.audioCall;
 const setup = QiscusMeetModule.setup;
+const getSetup = QiscusMeetModule.getSetup;
 
-QiscusMeetModule.call = (info) => {
+// Get Token 
+const getUserData = async ({
+  displayName = "",
+  email = "",
+  room = "",
+  avatar = "",
+}) => {
+  const { appId, url } = await getSetup();
+
+  const response = await fetch(`${url}:9090/generate_url`, {
+    method: "POST",
+    body: {
+      avatar: avatar,
+      name: displayName,
+      email: email,
+      iss: "meetcall",
+      sub: "call.qiscus.com",
+      room: room,
+      moderator: false,
+      appId: appId,
+    },
+  });
+
+  const data = await response.json();
+  return data;
+};
+
+QiscusMeetModule.call = async (info) => {
   const userInfo = info || {};
   const { videoMuted, audioMuted, room } = userInfo;
-  
-  if(!room) {
+
+  const data = await getUserData(userInfo);
+
+  if (!room) {
     console.log("Please provide room name");
     return;
   }
 
   if (!userInfo.audioOnly) {
-    call(userInfo, room, videoMuted, audioMuted);
+    call(userInfo, room, videoMuted, audioMuted, data.token);
   } else {
-    audioCall(userInfo,room, audioMuted);
+    audioCall(userInfo, room, audioMuted, data.token);
   }
 };
 
